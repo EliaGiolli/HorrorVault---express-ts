69ce2649fb739c2e38996355aa7f586963d99f65
Elia Giolli <eliagiolli22@gmail.com>
2025-10-26 11:10:25 +0100

    created index.js and server.js

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import helmet from 'helmet;'
+
+import { authMiddleware } from './middleware/authMiddleware';
+
+dotenv.config();
+
+// Configuration of the Express server
+export const app = express();
+app.use(helmet());
+app.use(express.json());
+
+// Connection to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('CONNECTED TO THE DATABASE'))
+    .catch(err => console.error("CONNECTION TO DB FAILED: ", err))
+
+// Protected route
+app.get("/protected", authMiddleware, (req, res) => {
+    res.json({ message: "Access granted!", user: req.user });
+});
+  
+// Other routes
+
+
